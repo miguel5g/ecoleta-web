86eca17bf14372f44247864927bad9ceb5cb13f3
Miguel Ângelo <miguelcg958@gmail.com>
2021-01-21 15:39:15 -0300

    update api

--- src/pages/api/items.ts
+++ src/pages/api/items.ts
@@ -1,16 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const items = [
-  { id: '01', title: 'Lâmpadas', image: 'lampadas.svg' },
-  { id: '02', title: 'Pilhas e Baterias', image: 'baterias.svg' },
-  { id: '03', title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
-  { id: '04', title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
-  { id: '05', title: 'Resíduos Orgânicos', image: 'organicos.svg' },
-  { id: '06', title: 'Óleo de Cozinha', image: 'oleo.svg' },
+  { id: 1, title: 'Lâmpadas', image: 'lampadas.svg' },
+  { id: 2, title: 'Pilhas e Baterias', image: 'baterias.svg' },
+  { id: 3, title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
+  { id: 4, title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
+  { id: 5, title: 'Resíduos Orgânicos', image: 'organicos.svg' },
+  { id: 6, title: 'Óleo de Cozinha', image: 'oleo.svg' },
 ];
 
 async function get(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json(items.map((item) => ({ ...item, imageUrl: `/uploads/${item.image}` })));
+  return res.status(200).json(items.map((item) => ({ ...item, image_url: `https://ecoleta-demo.vercel.app/uploads/${item.image}` })));
 }
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
